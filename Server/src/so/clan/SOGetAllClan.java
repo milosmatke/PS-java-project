bb4e65b4eb814097086e074a11a68fc9251554ed
Milos Matic <mmmatke999@gmail.com>
2025-09-03 00:15:37 +0200

    Uradjena pretraga clana.

---
+++ Server/src/so/clan/SOGetAllClan.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package so.clan;
+
+import db.DBBroker;
+import domain.AbstractDomainObject;
+import domain.Clan;
+import java.util.ArrayList;
+import so.AbstractSO;
+
+/**
+ *
+ * @author Korisnik
+ */
+public class SOGetAllClan extends AbstractSO{
+
+    private ArrayList<Clan> lista;
+
+    @Override
+    protected void validate(AbstractDomainObject ado) throws Exception {
+        if (!(ado instanceof Clan)) {
+            throw new Exception("Prosledjeni objekat nije instanca klase Clan!");
+        }
+    }
+
+    @Override
+    protected void execute(AbstractDomainObject ado) throws Exception {
+        ArrayList<AbstractDomainObject> clanovi = DBBroker.getInstance().select(ado);
+        lista = (ArrayList<Clan>) (ArrayList<?>) clanovi;
+    }
+
+    public ArrayList<Clan> getLista() {
+        return lista;
+    }
+    
+}
